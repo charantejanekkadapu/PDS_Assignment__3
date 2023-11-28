c8a9f364b9486f73a7af1c8ca61fe6e6227aecd0
charantejanekkadapu <146148043+charantejanekkadapu@users.noreply.github.com>
2023-11-27 22:53:24 -0600

    Update 1a.py

--- code/1a.py
+++ code/1a.py
@@ -2,17 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 diabetes_data = pd.read_csv('diabetes.csv')
-# Set a seed for reproducibility
 np.random.seed(35)
 
-# Take a random sample of 25 observations
 sample_data = diabetes_data.sample(n=25)
-
-# Calculate mean and highest Glucose values for the sample
 sample_mean_glucose = sample_data['Glucose'].mean()
 sample_highest_glucose = sample_data['Glucose'].max()
-
-# Calculate mean and highest Glucose values for the population
 population_mean_glucose = diabetes_data['Glucose'].mean()
 population_highest_glucose = diabetes_data['Glucose'].max()
 
